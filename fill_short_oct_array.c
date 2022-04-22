014bd39c09902d8ac0e88c6b91001fb270d24786
akinyiv <akinyivee10@gmail.com>
2022-04-22 19:51:56 +0300

    fill oct array

--- fill_short_oct_array.c
+++ fill_short_oct_array.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * fill_oct_array - writes the character c to stdout
+ * fill_short_oct_array - calculates a short octal number
  *
  * @bnr: array where is stored the binary.
  * @oct: array where is stored the octal.
  *
  * Return: binary array.
  */
-char *fill_oct_array(char *bnr, char *oct)
+char *fill_short_oct_array(char *bnr, char *oct)
 {
 	int op, i, j, ioct, limit;
 
-	oct[11] = '\0';
-	for (i = 31, ioct = 10; i >= 0; i--, ioct--)
+	oct[6] = '\0';
+	for (i = 15, ioct = 5; i >= 0; i--, ioct--)
 	{
-		if (i > 1)
+		if (i > 0)
 			limit = 4;
 		else
-			limit = 2;
+			limit = 1;
 		for (op = 0, j = 1; j <= limit; j *= 2, i--)
 			op = ((bnr[i] - '0') * j) + op;
 		i++;
